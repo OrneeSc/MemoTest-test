02a76788946296af768beeddc9bb6382e8c256df
Ornella <ornee.sci@gmail.com>
2020-08-22 20:32:39 -0300

    first commit

---
+++ Juego de la memoria/index.js
@@ -0,0 +1,14 @@
+const onCardClick = event => {
+    const cards = document.querySelectorAll(".card");
+    const 
+    if (cards == hidden)
+   
+    console.log(cards);
+
+}
+
+
+const load = () => {
+    const cards = document.querySelectorAll(".card");
+    cards.addEventListener("click", onCardClick);
+}

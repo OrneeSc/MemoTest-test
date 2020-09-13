adcce4fc9b7457ca7fe7a07dd16afcbc5e1127fb
Ornella <ornee.sci@gmail.com>
2020-09-13 15:02:03 -0300

    juego de la memoria falta terminar

--- Juego de la memoria/index.js
+++ Juego de la memoria/index.js
@@ -1,14 +1,153 @@
-const onCardClick = event => {
-    const cards = document.querySelectorAll(".card");
-    const 
-    if (cards == hidden)
-   
-    console.log(cards);
-
-}
-
-
 const load = () => {
-    const cards = document.querySelectorAll(".card");
-    cards.addEventListener("click", onCardClick);
+    //TEAL
+    const teal = document.querySelector("#buttonTeal");
+    teal.addEventListener("click", e => {
+        buttonTeal();
+    });
+
+    //PINK
+    const pink = document.querySelector("#buttonPink");
+    pink.addEventListener("click", e => {
+        buttonPink();
+    });
+
+    //GREEN
+    const green = document.querySelector("#buttonGreen");
+    green.addEventListener("click", e => {
+        buttonGreen();
+    });
+
+    //TEAL2
+    const teal2 = document.querySelector("#buttonTeal2");
+    teal2.addEventListener("click", e => {
+        buttonTeal2();
+    });
+
+    //GREY
+    const grey = document.querySelector("#buttonGrey");
+    grey.addEventListener("click", e => {
+        buttonGrey();
+    });
+
+    //YELLOW
+    const yellow = document.querySelector("#buttonYellow");
+    yellow.addEventListener("click", e => {
+        buttonYellow();
+    });
+
+    //PURPLE
+    const purple = document.querySelector("#buttonPurple");
+    purple.addEventListener("click", e => {
+        buttonPurple();
+    });
+
+    //GREY 2
+    const grey2 = document.querySelector("#buttonGrey2");
+    grey2.addEventListener("click", e => {
+        buttonGrey2();
+    });
+
+     //CRIMSON
+     const crimson = document.querySelector("#buttonCrimson");
+     crimson.addEventListener("click", e => {
+         buttonCrimson();
+    });
+
+    //PINK 2
+    const pink2 = document.querySelector("#buttonPink2");
+    pink2.addEventListener("click", e => {
+        buttonPink2();
+    });
+
+    //ORANGE
+    const orange = document.querySelector("#buttonOrange");
+    orange.addEventListener("click", e => {
+        buttonOrange();
+    });
+
+    //ORANGE 2
+    const orange2 = document.querySelector("#buttonOrange2");
+    orange2.addEventListener("click", e => {
+        buttonOrange2();
+    });
+
+    //PURPLE 2
+    const purple2 = document.querySelector("#buttonPurple2");
+    purple2.addEventListener("click", e => {
+        buttonPurple2();
+    });
+
+    //CRIMSON 2
+    const crimson2 = document.querySelector("#buttonCrimson2");
+    crimson2.addEventListener("click", e => {
+        buttonCrimson2();
+    });
+
+    //GREEN 2
+    const green2 = document.querySelector("#buttonGreen2");
+    green2.addEventListener("click", e => {
+        buttonGreen2();
+    });
+
+    //YELLOW 2
+    const yellow2 = document.querySelector("#buttonYellow2");
+    yellow2.addEventListener("click", e => {
+        buttonYellow2();
+    });
+
+    //BUTTON NOT ACTIVE
+   /* const button = document.querySelectorAll(".button");
+    button.addEventListener("click", e => {
+    });*/
+
+   
+};
+
+const buttonTeal = () => {
+    document.querySelector("#buttonTeal").style.backgroundColor = "#20c997";
+};
+const buttonPink = () => {
+    document.querySelector("#buttonPink").style.backgroundColor = "#e83e8c";
+};
+const buttonGreen = () => {
+    document.querySelector("#buttonGreen").style.backgroundColor = "#28a745";
+}
+const buttonTeal2 = () => {
+    document.querySelector("#buttonTeal2").style.backgroundColor = "#20c997";
+}
+const buttonGrey = () => {
+    document.querySelector("#buttonGrey").style.backgroundColor = "#6c757d";
+}
+const buttonYellow = () => {
+    document.querySelector("#buttonYellow").style.backgroundColor = "#ffc107";
+}
+const buttonPurple = () => {
+    document.querySelector("#buttonPurple").style.backgroundColor = "#6f42c1";
+}
+const buttonGrey2 = () => {
+    document.querySelector("#buttonGrey2").style.backgroundColor = "#6c757d";
+}
+const buttonCrimson = () => {
+    document.querySelector("#buttonCrimson").style.backgroundColor = "#dc143c";
+}
+const buttonPink2 = () => {
+    document.querySelector("#buttonPink2").style.backgroundColor = "#e83e8c";
+}
+const buttonOrange = () => {
+    document.querySelector("#buttonOrange").style.backgroundColor = "#fd7e14";
+}
+const buttonOrange2 = () => {
+    document.querySelector("#buttonOrange2").style.backgroundColor = "#fd7e14";
+}
+const buttonPurple2 = () => {
+    document.querySelector("#buttonPurple2").style.backgroundColor = "#6f42c1";
+}
+const buttonCrimson2 = () => {
+    document.querySelector("#buttonCrimson2").style.backgroundColor = "#dc143c";
+}
+const buttonGreen2 = () => {
+    document.querySelector("#buttonGreen2").style.backgroundColor = "#28a745";
+}
+const buttonYellow2 = () => {
+    document.querySelector("#buttonYellow2").style.backgroundColor = "#ffc107";
 }
